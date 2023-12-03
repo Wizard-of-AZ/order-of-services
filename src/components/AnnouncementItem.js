c07a77d7b33406c23e4ebd918ec8b1638cc3e9a9
Brandon Kent <bkent22@apple.com>
2023-12-03 09:09:37 -0700

    fix

--- src/components/AnnouncementItem.js
+++ src/components/AnnouncementItem.js
@@ -1,21 +1,11 @@
 import { v4 } from 'uuid'
 
 export default function AnnouncementItem({ organization, announcements, link }) {
-  const lastAnnouncement = announcements.length - 1;
   return (
     <div>
       <hr className="h-px my-4 bg-gray-400 border-0" />
       <h2 className="mb-2 text-lg font-bold text-gray-900 text-center">{organization}</h2>
-        {(announcements.length > 0) &&
-        announcements.map((announcement, index) =>
-          <div className="text-center" key={v4()}>
-            <div className="pb-4">
-              {announcement}
-              {/* TODO: nuke me. */}
-              {/* {index !== lastAnnouncement && <div className="text-center">✵ ✵ ✵</div>} */}
-            </div>
-          </div>
-        )}
+      <div className="text-center pb-4">{announcements}</div>
       {/* TODO: Make routeLink more generic to use here. */}
       {link && (
         <a className="underline my-1 font-route text-sky-600 font-semibold" href={link} target="_blank" rel="noreferrer">{link}</a>
