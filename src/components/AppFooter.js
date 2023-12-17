5a1609cac52c75458e3b04d29e4b2e87d05d8ae3
sikkdev <brandonk3nt@gmail.com>
2023-12-17 07:44:32 -0700

    sunday

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -9,10 +9,10 @@ export default function AppFooter({ currentRoute }) {
       <hr className="h-px my-4 bg-gray-400 border-0" />
       <div className="py-2 mb-8 text-center">
         {currentRoute !== '/services' && <RouteLink route="/services" text="Back To Order of Services" />}
-        {currentRoute === '/services' && <RouteLink route="/announcements" text="View Announcements" />}
+        {/* {currentRoute === '/services' && <RouteLink route="/announcements" text="View Announcements" />} */}
         {/* {currentRoute === '/services' && <RouteLink route="/about" text="View Ward Leadership" />} */}
         {/* {currentRoute === '/announcements' && <RouteLink url={circlesUrl} text="Join The Conversation In Circles" />} */}
-        {currentRoute === '/announcements' && <RouteLink url="https://www.churchofjesuschrist.org/calendar/month" text="View Ward Calendar" />}
+        {/* {currentRoute === '/announcements' && <RouteLink url="https://www.churchofjesuschrist.org/calendar/month" text="View Ward Calendar" />} */}
         {/* {currentRoute === '/announcements' && <RouteLink url="https://discord.gg/QmUrmzUxCt" text="Join The Conversation On Discord" />} */}
       </div>
     </div>
